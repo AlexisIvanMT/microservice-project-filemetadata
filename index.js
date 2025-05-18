afa605bad26ced590cb387d0fbd370b7a0bd23d5
Alexis Morales <alexis.ivan.mt@gmail.com>
2025-05-17 23:56:31 -0600

    added funtion fileanalyse

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
@@ -11,7 +13,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+  res.json(fileInfo);
+});
 
 
 const port = process.env.PORT || 3000;
